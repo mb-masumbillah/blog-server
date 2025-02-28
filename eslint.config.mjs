32eb6b194a11f6658457171d9af424f8ee26664d
Masum Billah <masumbillah2062003@gmail.com>
2025-02-28 12:05:36 +0600

    added blog server setup and modulation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,31 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
   {
     languageOptions: {
       globals: {
-        process: "readonly",
+        process: 'readonly',
         ...globals.browser,
-        ...globals.node
-      }
-    }
+        ...globals.node,
+      },
+    },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
     rules: {
-      "no-unused-vars": "error",
-      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
-      "no-unused-expressions": "error",
-      "no-console": "warn",
-      "no-undef": "error",
+      'no-unused-vars': 'error',
+      'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+      'no-unused-expressions': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
     },
-
   },
   {
-    ignores: [
-      "node_modules/*",
-      "dist/*",
-    ]
+    ignores: ['node_modules/*', 'dist/*'],
   },
 ];
